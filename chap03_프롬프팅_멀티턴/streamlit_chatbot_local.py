d5dd5a79936116410e353577ace92fe339355c18
진정환 <wjdghks4423@gmail.com>
2026-04-15 12:00:16 +0900

    로컬파일 추가

--- chap03_프롬프팅_멀티턴/streamlit_chatbot_local.py
+++ chap03_프롬프팅_멀티턴/streamlit_chatbot_local.py
@@ -1,7 +1,13 @@
 import streamlit as st
 from google import genai
 from google.genai import types
+from dotenv import load_dotenv
+import os
 
+'''
+load_dotenv()
+gemini_api_key = os.getenv('GOOGLE_API_KEY')
+'''
 api_key = st.secrets["GEMINI_API_KEY"]
 client = genai.Client(api_key=api_key)
 
